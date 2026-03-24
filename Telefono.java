129addb27ea358ee84a7d141c1e47284e803163c
Janiry Rodriguez <janiryrodriguez6@gmail.com>
2026-03-24 11:41:15 -0600

    inicio proyecto

---
+++ Telefono.java
@@ -0,0 +1,10 @@
+class Telefono{
+    public String forma;
+    private Integer bateria;
+    protected String camara;
+
+    public String vender () {
+    }
+    private void reproducirVideo () {
+    }
+}
