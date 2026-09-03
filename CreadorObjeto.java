6a31b0721698c8f815530fd54e5c7bda483d8226
Janiry Rodriguez <janiryrodriguez6@gmail.com>
2026-09-02 20:49:00 -0600

    CreadorObjeto

--- CreadorObjeto.java
+++ CreadorObjeto.java
@@ -1,5 +1,18 @@
 class CreadorObjeto {
-    public static void main(String[] args){
+     public static void main(String[] args){
+        MascotaVirtual [] refugio={
+         new Perro ("Rocky"),
+         new Gato ("Michi"),
+         new Perro ("Kira"),
+         new  Gato ("Tuluz"),
+         new Pollito("Lalo")
+        };
 
-    }
+        for(MascotaVirtual m:refugio){
+         m.hacerSonido();
+         m.mostrarEstado();
+
+        }
+    }   
+
 }
